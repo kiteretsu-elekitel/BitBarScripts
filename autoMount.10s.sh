cef979dbb4783c9b4d788015054b1b343136e560
kiteretsu-elekitel <kiteretsu.elekitel@gmail.com>
2017-11-26 01:44:37 +0900

    change file name, and change 'umount' to 'diskutil unmount'

--- autoMount.10s.sh
+++ autoMount.10s.sh
@@ -7,13 +7,14 @@ if [[ $stat = 'Yes' ]]; then
 		exit 0
 	else
 		osascript -e "try" -e "mount volume \"smb://teapot\"" -e "end try"
+		echo "Mount teapot samba disk"
 	fi
 else
 	if [[ -d /Volumes/OrangePekoe ]]; then
-		umount /Volumes/OrangePekoe
+		diskutil unmount /Volumes/OrangePekoe
 		if [[ $? = '1' ]]; then
 			killall iTunes
-			umount /Volumes/OrangePekoe
+			diskutil unmount /Volumes/OrangePekoe
 		fi
 	else
 		exit 0
